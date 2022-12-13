a687c349ef84a7775e9b7bf20c56895c4c010625
大剑 <70927349+cl4ym0re@users.noreply.github.com>
2022-12-13 15:01:54 +0800

    Add files via upload

---
+++ shellcode_xor.py
@@ -0,0 +1,45 @@
+##!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# author(cn):大剑
+# author(en):cl4ym0re
+
+import argparse
+
+
+def bannerPrint():
+    banner = r'''
+██╗  ██╗ ██████╗ ██████╗ 
+╚██╗██╔╝██╔═══██╗██╔══██╗
+ ╚███╔╝ ██║   ██║██████╔╝
+ ██╔██╗ ██║   ██║██╔══██╗
+██╔╝ ██╗╚██████╔╝██║  ██║
+╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝
+    why not xor it                                  
+    '''
+    print(banner)
+
+def xor(num):
+    key = num;
+    input_file = "payload.bin"
+    output_file = "encrypt.bin"
+
+    with open(input_file, "rb") as f:
+        payload = f.read()
+        encrypted_payload = bytearray(len(payload))
+
+    for i in range(len(payload)):
+        encrypted_payload[i] = payload[i] ^ num
+
+    with open(output_file, "wb") as f:
+        f.write(encrypted_payload)
+    print("[*]Saved as encrypt bin.")
+if __name__ == "__main__":
+    bannerPrint()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n","--num",help="Xor key",type=int)
+    args = parser.parse_args()
+    if args.num != None:
+        xor(args.num)
+    else:
+        print("[-]XOR key is not provided.")
+        print("[?]usage: python3 shellcode_xor.py -n 99")
